b5567b1f3b043e2d199f3f6c4a25305fde2c51dd
Mina Wissa <mina-samy@hotmail.com>
2021-02-24 22:43:10 +0100

    create packages, display fragment and update git ignore

--- app/src/main/java/com/vimcar/vehicletracker/ui/VehicleOverviewFragment.kt
+++ app/src/main/java/com/vimcar/vehicletracker/ui/VehicleOverviewFragment.kt
@@ -1,18 +1,19 @@
-package com.vimcar.vehicletracker
+package com.vimcar.vehicletracker.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.vimcar.vehicletracker.R
 
-class MapFragment : Fragment() {
+class VehicleOverviewFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        return inflater.inflate(R.layout.fragment_vehicle_overview, container, false)
     }
 }

7d4b08f4a7722cc6e813c7f2eb3415417f826f9c
Mina Wissa <mina-samy@hotmail.com>
2021-03-01 00:45:19 +0100

    fix test

--- app/src/test/java/com/vimcar/vehicletracker/data/repository/VehiclesRepositoryTest.kt
+++ app/src/test/java/com/vimcar/vehicletracker/data/repository/VehiclesRepositoryTest.kt
@@ -1,7 +1,10 @@
 package com.vimcar.vehicletracker.data.repository
 
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.never
+import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
+import com.vimcar.vehicletracker.data.local.VehiclesCache
 import com.vimcar.vehicletracker.data.remote.api.VehicleApi
 import com.vimcar.vehicletracker.vehiclesApiResponse
 import com.vimcar.vehicletracker.vehiclesList
@@ -11,15 +14,31 @@ import org.junit.Test
 class VehiclesRepositoryTest {
 
     private val api: VehicleApi = mock()
-    private val repository = VehiclesRepositoryImpl(api)
+    private val cache: VehiclesCache = mock()
+    private val repository = VehiclesRepositoryImpl(api, cache)
 
     @Test
-    fun `when api returns data should return vehicles list`() {
+    fun `when cache is empty should call api and store in cache`() {
         whenever(api.getVehicles()).thenReturn(Single.just(vehiclesApiResponse))
+        whenever(cache.getVehicles()).thenReturn(emptyList())
 
         repository.getVehicles()
             .test()
             .assertValue(vehiclesList)
             .assertTerminated()
+
+        verify(cache).storeVehicles(vehiclesApiResponse)
+    }
+
+    @Test
+    fun `when cache is not empty should return from cache`() {
+        whenever(cache.getVehicles()).thenReturn(vehiclesApiResponse)
+
+        repository.getVehicles()
+            .test()
+            .assertValue(vehiclesList)
+            .assertTerminated()
+
+        verify(api, never()).getVehicles()
     }
 }
